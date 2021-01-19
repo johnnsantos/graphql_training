bbc10d862a8ad00b60606d61f88f62f748252dd4
Johnnatan <contatojohnnatan@gmail.com>
2021-01-18 22:13:04 -0300

    feat: get api data with graphql by apollo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [page, setPage] = useState(1);
+
+  const ALL_CHARACTERS = gql`
+    {
+      characters(page: ${page}
+  ) {
+        info {
+          next
+          prev
+        }
+        results {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(ALL_CHARACTERS);
+
+  const nextPage = () => {
+    if (data.characters.info.next) {
+      setPage(data.characters.info.next);
+    }
+  };
+  const previousPage = () => {
+    if (data.characters.info.prev) {
+      setPage(data.characters.info.prev);
+    }
+  };
+
+  if (loading)
+    return (
+      <div className="App">
+        <p>Carregando</p>
+      </div>
+    );
+  if (error)
+    return (
+      <div className="App">
+        <p>Erro!</p>
+      </div>
+    );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Personagens Rick Morty</h3>
+
+      <ul>
+        {data.characters.results.map((char, index) => (
+          <li key={index}>{char.name}</li>
+        ))}
+      </ul>
+      <div>
+        <button onClick={previousPage}>Página anterior</button>
+        <button onClick={nextPage}>Próxima página</button>
+      </div>
     </div>
   );
 }
-
-export default App;
